c1aacc3a284a93d47d06a4ce4d7d64862ef7569c
Vincent Goudket <vincent.goudket@X9CQ02HK96.io-internal.net>
2023-05-25 16:37:12 +0200

    Add all lessons done on 25/05

---
+++ episode-07/blog/resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+    <body>
+
+        <article>
+            <h1><a href="/post">My First Post</a></h1>
+                 <div></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat dolore illum nihil. Illum, facere itaque? Recusandae impedit saepe explicabo suscipit neque nihil error, quod, nostrum doloremque deserunt laboriosam quisquam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat dolore illum nihil. Illum, facere itaque? Recusandae impedit saepe explicabo suscipit neque nihil error, quod, nostrum doloremque deserunt laboriosam quisquam.</p>
+        </article>
+
+        <a href="/">Go back</a>
+
+    </body>
---
+++ episode-07/blog/resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+    <body>
+
+        <article>
+            <h1><a href="/post">My First Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat dolore illum nihil. Illum, facere itaque? Recusandae impedit saepe explicabo suscipit neque nihil error, quod, nostrum doloremque deserunt laboriosam quisquam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat dolore illum nihil. Illum, facere itaque? Recusandae impedit saepe explicabo suscipit neque nihil error, quod, nostrum doloremque deserunt laboriosam quisquam.</p>
+        </article>
+
+        <article>
+            <h1><a href="/post">My Second Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat dolore illum nihil. Illum, facere itaque? Recusandae impedit saepe explicabo suscipit neque nihil error, quod, nostrum doloremque deserunt laboriosam quisquam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat dolore illum nihil. Illum, facere itaque? Recusandae impedit saepe explicabo suscipit neque nihil error, quod, nostrum doloremque deserunt laboriosam quisquam.</p>
+        </article>
+
+        <article>
+            <h1><a href="/post">My Third Post</a></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat dolore illum nihil. Illum, facere itaque? Recusandae impedit saepe explicabo suscipit neque nihil error, quod, nostrum doloremque deserunt laboriosam quisquam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat dolore illum nihil. Illum, facere itaque? Recusandae impedit saepe explicabo suscipit neque nihil error, quod, nostrum doloremque deserunt laboriosam quisquam.</p>
+        </article>
+
+    </body>
